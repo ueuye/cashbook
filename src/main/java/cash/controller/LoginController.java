67f297ba7ffbd8cad96cda1d3b21257e27ac0e4d
ueuye <eu99ne@gmail.com>
2023-07-07 17:40:42 +0900

    로그인 - 쿠키 기능구현,  css 적용

--- src/main/java/cash/controller/LoginController.java
+++ src/main/java/cash/controller/LoginController.java
@@ -3,6 +3,7 @@ package cash.controller;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +13,21 @@ import cash.model.MemberDao;
 import cash.vo.Member;
 
 @SuppressWarnings("serial")
-@WebServlet("/login")
+@WebServlet("/off/login")
 public class LoginController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// session 인증 검사 코드
-		HttpSession session = request.getSession();
-		if (session.getAttribute("loginMember") != null) {
-	         response.sendRedirect(request.getContextPath() + "/calendar");
-	         return;
-	      }
+		// 쿠키에 저장된 아이디가 있다면 request속성에 저장 
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for(Cookie c: cookies) {
+				if(c.getName().equals("loginId")) {
+					request.setAttribute("loginId", c.getValue());
+				}
+			}
+		}
+		
 		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 	}
 	
@@ -40,13 +45,21 @@ public class LoginController extends HttpServlet {
 		
 		if(loginMember == null) { // null이면 로그인 실패
 			System.out.println("로그인 실패");
-			response.sendRedirect(request.getContextPath()+"/login");
+			response.sendRedirect(request.getContextPath()+"/off/login");
 			return;
 		}else { // 로그인 성공 : session사용
 			HttpSession session = request.getSession();
 			System.out.println("로그인 성공");
 			session.setAttribute("loginMember", loginMember);
-			response.sendRedirect(request.getContextPath()+"/calendar");
+			
+			// idSave 체크값이 넘어 왔다면 쿠키에 저장
+			if(request.getParameter("idSave") != null) {
+				Cookie loginIdcookie =  new Cookie("loginId", memberId);
+				loginIdcookie.setMaxAge(60*60*24); // 하루
+				response.addCookie(loginIdcookie);
+			}
+			
+			response.sendRedirect(request.getContextPath()+"/on/calendar");
 		}
 		
 	}
